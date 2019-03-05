0aae666c342c03b9d982e8b1079ceaddbd3e68b2
pablo.mint <slpkvol3@gmail.com>
2019-03-05 11:06:39 -0300

    Add LowLightLamp

---
+++ LowLightLamp/LowLightLamp.ino
@@ -0,0 +1,53 @@
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightMeter;
+
+/*
+Connection:
+ VCC-5v
+ GND-GND
+ SCL-SCL(analog pin 5)
+ SDA-SDA(analog pin 4)
+ ADD-NC or GND
+*/
+
+int relayOutputPin = 12;
+
+const int lowLightCountTrigger = 10; // minimum low light events needed to trigger the lamp
+const int lowLightLevel = 3; // analog read level at which low light events are triggered
+
+int lowLightCount = 0; // count for the low light events
+
+void setup(){
+  // Serial.begin(9600);
+  lightMeter.begin();
+  lightMeter.configure(0x10);
+
+  pinMode(relayOutputPin, OUTPUT);
+  digitalWrite(relayOutputPin, LOW);
+}
+
+void loop() {
+  uint16_t lux = lightMeter.readLightLevel();
+
+  //count low light level events
+  if (lux < lowLightLevel) {
+    if (lowLightCount <= lowLightCountTrigger) {
+      lowLightCount = lowLightCount + 1;
+    }
+  } else {
+    if (lowLightCount > 0) {
+      lowLightCount = lowLightCount - 1;
+    }
+  }
+
+  // turn on light when enough low light level events have been detected
+  if (lowLightCount >= lowLightCountTrigger) {
+    digitalWrite(relayOutputPin, HIGH);
+  } else if (lowLightCount == 0) {
+    digitalWrite(relayOutputPin, LOW);
+  }
+
+  delay(1000);
+}
---
+++ LowLightLamp/README.md
@@ -0,0 +1,13 @@
+# LowLightSensor
+
+A low light sensor that activates a relay whenever low light is detected. A night lamp, if you will...
+There's an alternative version for use with an LDR instead of a light sensor module.
+The LDR seems to be more robust against the external forces of nature.
+The container obviously needs to have a hole in it for the light sensor to do it's job.
+Make sure to double seal it to protect the innards from water, and regularly check it for UV light degradation.
+
+## Parts list
+
+- 1x BH1750 light sensor module
+- 1x Arduino Pro Mini
+- 1x Relay module
